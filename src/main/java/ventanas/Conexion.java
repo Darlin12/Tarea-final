c46841a0f283869495009734e50688a1bcdf9610
Darlin <darlinmanuelcasado@gmail.com>
2021-12-10 12:40:43 -0800

    segundo commit

--- src/main/java/ventanas/Conexion.java
+++ src/main/java/ventanas/Conexion.java
@@ -6,10 +6,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-/** 
- *
- * @author darli
- */
+
 public class Conexion {
     
      Connection cn = null;
